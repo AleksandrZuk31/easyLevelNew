d3b30fb6b630ce6d5f75ba9a1e565ed7489a2a28
Aleksand Zuk <Aleksandrzuk30013010@gmail.com>
2021-07-17 18:34:31 +0300

    еще подправил

--- src/modules/smoothScroll.js
+++ src/modules/smoothScroll.js
@@ -1,18 +1,27 @@
 const smoothScroll = () => {
     const arrowUp = document.querySelector('.up');
 
-    arrowUp.addEventListener('click', (e) => {
-        window.scrollTo(0, 600);
-        arrowUp.style.display = 'none';
+    arrowUp.onclick = () => {
+        window.scrollTo(pageXOffset, 90);
+    };
+
+    window.addEventListener('scroll', () => {
+        arrowUp.hidden = (pageYOffset < document.documentElement.clientHeight);
     });
 
-    window.addEventListener('wheel', (e) => {
-        if (e.deltaY > 0) {
-            arrowUp.style.display = 'block';
-        } else {
-            arrowUp.style.display = 'none';
-        }
-    });
+
+    // arrowUp.addEventListener('click', (e) => {
+    //     window.scrollTo(0, 600);
+    //     arrowUp.style.display = 'none';
+    // });
+
+    // window.addEventListener('wheel', (e) => {
+    //     if (e.deltaY > 0) {
+    //         arrowUp.style.display = 'block';
+    //     } else {
+    //         arrowUp.style.display = 'none';
+    //     }
+    // });
 };
 
 export default smoothScroll;
