481bb1a31b18bae39bd7945f3fa59e783716460b
Aleksand Zuk <Aleksandrzuk30013010@gmail.com>
2021-07-17 09:12:20 +0300

    убрал срабатывание стандартных ссылок

--- src/modules/moveSection.js
+++ src/modules/moveSection.js
@@ -2,6 +2,7 @@ const moveSection = () => {
     const topMenu = document.querySelector('.top-menu');
 
     topMenu.addEventListener('click', () => {
+        event.preventDefault();
         let target = event.target;
         if (target.matches('a')) {
             const thisLink = document.querySelector(target.getAttribute("href"));
--- src/modules/smoothScroll.js
+++ src/modules/smoothScroll.js
@@ -1,9 +1,42 @@
 const smoothScroll = () => {
     const arrowUp = document.querySelector('.up');
 
-    // const sections = [...document.getElementsByTagName('section')];
-    // let currentSection = 0;
+    const sections = [...document.getElementsByTagName('section')];
+    let currentSection = 0;
+    console.log(sections);
 
+    window.addEventListener('wheel', function (e) {
+
+
+        if (e.deltaY < 0) {
+            --currentSection;
+            console.log(1);
+        } else {
+            ++currentSection;
+            console.log(2);
+        }
+
+        if (currentSection < 0) {
+            currentSection = 0;
+        } else if (currentSection > (sections.length - 1)) {
+            currentSection = (sections.length - 1);
+        }
+
+       // scrollToSection(currentSection);
+    });
+
+
+// const rock = document.getElementsByTagName('section');
+// console.log(rock);
+//  function scrollToSection(i) {
+//      console.log(i);
+//         document.getElementsByTagName('sections'[i]).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+
+  //  };
+
+};
 
     // window.addEventListener('wheel', function (e) {
     //     e.preventDefault();
@@ -40,6 +73,6 @@ const smoothScroll = () => {
 
 
 
-};
+
 
 export default smoothScroll;
